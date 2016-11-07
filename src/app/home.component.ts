203911f25fa51236765150844f741d697f4fefc4
Mojtaba Ghp <mat.ghp@gmail.com>
2016-11-07 15:22:32 +0100

    HomeComponent added

---
+++ src/app/home.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'os-home',
+  template: `
+  <hi>Home Page goes here...!</hi>
+`,
+  styles: [``]
+})
+export class HomeComponent  {
+
+}
